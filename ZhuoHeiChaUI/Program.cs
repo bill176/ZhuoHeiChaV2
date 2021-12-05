e6f481db744c2eecc6293306c2bdac3b736508cd
Bill Zhang <bill.nonbusiness@gmail.com>
2021-12-05 16:42:17 -0500

    created a UI shortcut for testing room API

---
+++ ZhuoHeiChaUI/Program.cs
@@ -0,0 +1,26 @@
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZhuoHeiChaUI
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("app");
+
+            // TODO: refactor the IP address into config
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:7071") });
+
+            await builder.Build().RunAsync();
+        }
+    }
+}
