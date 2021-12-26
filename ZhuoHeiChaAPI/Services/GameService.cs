15b8ce3825ef5ca5160b014e5e43e96955589f81
Bill Zhang <bill.nonbusiness@gmail.com>
2021-12-26 09:33:00 -0500

    implemented SendCards in GameService

--- ZhuoHeiChaAPI/Services/GameService.cs
+++ ZhuoHeiChaAPI/Services/GameService.cs
@@ -1,17 +1,65 @@
-﻿using System;
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using ZhuoHeiChaCore;
 
 namespace ZhuoHeiChaAPI.Services
 {
     public class GameService : IGameService
     {
+        private readonly ConcurrentDictionary<int, (IGame, object)> _gameSessions = new ConcurrentDictionary<int, (IGame, object)>();
 
+        private readonly ILogger _logger;
+        private readonly ICardHelper _cardHelper;
+
+        public GameService(ILogger logger, ICardHelper cardHelper)
+        {
+            _logger = logger;
+            _cardHelper = cardHelper;
+        }
+
+
+        public bool SendCards(int gameId, int sourcePlayerId, int targetPlayerId, IEnumerable<int> cardIds, out Dictionary<int, IEnumerable<Card>> updatedPlayerCards)
+        {
+            updatedPlayerCards = null;
+
+            // check if game id is valid
+            if (!_gameSessions.TryGetValue(gameId, out var gameLockPair))
+            {
+                _logger.LogError($"{gameId} is not a valid game id!");
+                return false;
+            }
+
+            // check if card ids are valid
+            var cards = _cardHelper.ConvertIdsToCards(cardIds);
+            if (cards == null)
+            {
+                _logger.LogError($"{string.Join(',', cardIds)} contains invalid cards");
+                return false;
+            }
+
+            // perform the card change
+            var (game, lockObject) = gameLockPair;
+            lock (lockObject)
+            {
+                try
+                {
+                    updatedPlayerCards = game.SendCards(sourcePlayerId, targetPlayerId, cards);
+                }
+                catch (Exception e)
+                {
+                    _logger.LogError(e, "Unable to send cards!");
+                    return false;
+                }
+            }
+
+            return true;
+        }
     }
 
     public interface IGameService
     {
-
+        bool SendCards(int gameId, int sourcePlayerId, int targetPlayerId, IEnumerable<int> cardIds, out Dictionary<int, IEnumerable<Card>> updatedPlayerCards);
     }
 }
