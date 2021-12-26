000782448d70d3fe851a997d1b7d6d5ba2f0e1d5
Bill Zhang <bill.nonbusiness@gmail.com>
2021-12-26 12:21:10 -0500

    add GameTest placeholder

---
+++ ZhuoHeiChaCore.Test/GameTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Xunit;
+using Moq;
+using ZhuoHeiChaCore;
+
+namespace ZhuoHeiChaCore.Test
+{
+    public class GameTest
+    {
+        [Fact]
+        public void SendCards_WhenPlayerIdsAreNotValid_ShouldThrowException()
+        {
+
+        }
+
+        [Fact]
+        public void SendCards_WhenPlayerDoesntHaveCards_ShouldThrowException()
+        {
+
+        }
+
+        [Fact]
+        public void SendCards_WhenPlayerIdsAndCardsAreValid_ShouldReturnUpdatedPlayerCards()
+        {
+
+        }
+    }
+}
