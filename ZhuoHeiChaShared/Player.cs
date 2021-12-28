b5c7ce713edd465d0d3b2da78aaadb8af934df17
Bill Zhang <bill.nonbusiness@gmail.com>
2021-12-28 11:05:31 -0500

    cleaned up the name of ZhuoHeiChaShared project

---
+++ ZhuoHeiChaShared/Player.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace ZhuoHeiChaShared
+{
+    public class Player
+    {
+        public string ConnectionId { get; set; }
+        public string Name { get; set; }
+    }
+}
